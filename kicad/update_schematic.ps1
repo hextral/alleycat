de67c8fd4d58a9985e279176f021f042c9660c94
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2022-09-06 16:56:42 -0700

    WICKED BASED NEW LAYOUT POGGERS

--- kicad/update_schematic.ps1
+++ kicad/update_schematic.ps1
@@ -1,2 +1,2 @@
-Copy-Item .\alleycat.kicad_sch .\alleycat_left.kicad_sch
-Copy-Item .\alleycat.kicad_sch .\alleycat_right.kicad_sch
+Copy-Item .\alleycat.kicad_sch .\left\alleycat_left.kicad_sch
+Copy-Item .\alleycat.kicad_sch .\right\alleycat_right.kicad_sch
